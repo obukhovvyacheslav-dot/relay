7c9d3565cb0aa55af2add2ba1535855008d08507
obukhovvyacheslav-dot <obukhov.vyacheslav@gmail.com>
2026-02-10 21:44:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, WebSocket
+import json
 
 app = FastAPI()
 
@@ -6,14 +7,17 @@ app = FastAPI()
 async def ws(ws: WebSocket):
     await ws.accept()
     while True:
-        await ws.receive_text()
+        data = await ws.receive_text()
+        print(data)
 
 @app.get("/voice")
 def voice():
     return """<?xml version="1.0" encoding="UTF-8"?>
 <Response>
   <Connect>
-    <ConversationRelay url="wss://relay-ug27.onrender.com/ws" />
+    <ConversationRelay
+      url="wss://relay-ug27.onrender.com/ws"
+      welcomeGreeting="Say something"
+    />
   </Connect>
-
 </Response>"""
